8aa5162d40627fbaedfe484fd2761e8f93ec4b0c
ariel <ariellira.al@gmail.com>
2023-12-07 23:23:29 -0800

    finished Day7

---
+++ 2023/Day7/Day7.Common/Solution.cs
@@ -0,0 +1,69 @@
+namespace Day7.Common;
+
+public static class Solution
+{
+    public static int PartOne(string input)
+    {
+        var numbers = input
+            .Split(Environment.NewLine)
+            .Where(line => line.Count() > 0)
+            .Select(line =>
+            {
+                int colonIndex = line.IndexOf(':') + 1;
+                return line[colonIndex..]
+                    .Trim()
+                    .Split(" ")
+                    .Where(line => line.Count() > 0)
+                    .Select(str => int.Parse(str));
+            });
+
+        var times = numbers.First().ToList();
+        var distances = numbers.Last().ToList();
+        var waysToWinForTimes = new List<int>();
+
+        for (int i = 0; i < times.Count() && i < distances.Count(); i++)
+        {
+            var time = times[i];
+            var distance = distances[i];
+            var waysToWin = 0;
+            for (int speed = 0; speed < time; ++speed)
+            {
+                var calculatedDistance = Math.Abs(time - speed) * speed;
+                if (calculatedDistance > distance)
+                {
+                    waysToWin += 1;
+                }
+            }
+            waysToWinForTimes.Add(waysToWin);
+        }
+
+        return waysToWinForTimes.Aggregate((acc, curr) => acc * curr);
+    }
+
+    public static int PartTwo(string input)
+    {
+        var numbers = input
+            .Split(Environment.NewLine)
+            .Where(line => line.Count() > 0)
+            .Select(line =>
+            {
+                int colonIndex = line.IndexOf(':') + 1;
+                return long.Parse(line[colonIndex..].Trim().Replace(" ", ""));
+            });
+
+        var time = numbers.First();
+        var distance = numbers.Last();
+
+        var waysToWin = 0;
+        for (int speed = 0; speed < time; ++speed)
+        {
+            var calculatedDistance = Math.Abs(time - speed) * speed;
+            if (calculatedDistance > distance)
+            {
+                waysToWin += 1;
+            }
+        }
+
+        return waysToWin;
+    }
+}
---
+++ 2023/Day7/Day7.Testing/UnitTest1.cs
@@ -0,0 +1,23 @@
+namespace Day7.Testing;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        var input =
+            @"Time:      7  15   30
+Distance:  9  40  200";
+
+        Assert.Equal(288, Common.Solution.PartOne(input));
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var input = @"Time:      7  15   30
+Distance:  9  40  200";
+
+        Assert.Equal(71503, Common.Solution.PartTwo(input));
+    }
+}
