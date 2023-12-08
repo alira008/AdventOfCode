50dcd5ba88923c7c10f84f72c36446f06cea566b
Ariel Lira <ariellira.al@gmail.com>
2023-12-08 14:31:08 -0800

    started Day8

---
+++ 2023/Day8/Day8.Common/Solution.cs
@@ -0,0 +1,37 @@
+namespace Day8.Common;
+
+public static class Solution
+{
+    enum CardType
+    {
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King,
+        Ace
+    }
+
+    static bool IsCardStrongerThan(char card, char other)
+    {
+    if
+        return false;
+    }
+
+    public static int PartOne(string input)
+    {
+        return -1;
+    }
+
+    public static int PartTwo(string input)
+    {
+        return -1;
+    }
+}
